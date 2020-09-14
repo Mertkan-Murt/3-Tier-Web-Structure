b800f9282dfd632b5c6bae781fd42bc192605ff6
jama1981 <zimp@inbox.ru>
2020-09-13 20:47:41 -0400

    Jamshid

--- Web-tier/main.tf
+++ Web-tier/main.tf
@@ -148,7 +148,7 @@ resource "aws_security_group" "Bastion-ssh-sg" {
     }
     vpc_id = aws_vpc.Project-VPC.id
     tags = {
-        Name = "HTTP SSH SG"
+        Name = "Bastion SSH SG"
     }
 }
 resource "aws_security_group" "web-http-ssh-sg" {
@@ -270,6 +270,62 @@ resource "aws_network_acl" "Web-ACL" {
     }
     tags = {
         Name = "Web-ACL"
+  }
+}
+
+resource "aws_network_acl" "App-ACL" {
+    vpc_id = aws_vpc.Project-VPC.id
+    subnet_ids = [aws_subnet.private-subnet-3.id, aws_subnet.private-subnet-4.id]
+    ingress {
+        protocol   = "-1"
+        rule_no    = 50
+        action     = "allow"
+        cidr_block = "0.0.0.0/0"
+        from_port  = 0
+        to_port    = 0
+    }
+    ingress {
+        protocol   = "tcp"
+        rule_no    = 100
+        action     = "allow"
+        cidr_block = "0.0.0.0/0"
+        from_port  = 80
+        to_port    = 80
+    }
+     ingress {
+        protocol   = "tcp"
+        rule_no    = 200
+        action     = "allow"
+        cidr_block = "0.0.0.0/0"
+        from_port  = 22
+        to_port    = 22
+    }
+    ingress {
+        protocol   = "tcp"
+        rule_no    = 300
+        action     = "allow"
+        cidr_block = "0.0.0.0/0"
+        from_port  = 32768
+        to_port    = 65353
+    }
+    ingress {
+        protocol   = "tcp"
+        rule_no    = 400
+        action     = "allow"
+        cidr_block = "0.0.0.0/0"
+        from_port  = 443
+        to_port    = 443
+    }
+    egress {
+        protocol   = "-1"
+        rule_no    = 100
+        action     = "allow"
+        cidr_block = "0.0.0.0/0"
+        from_port  = 0
+        to_port    = 0
+    }
+    tags = {
+        Name = "App-ACL"
   }
 }
 
